cfed04ce96a739e2980c993f8f59408aa803c248
mofris <loveinsemarang@gmail.com>
2025-04-25 19:45:48 +0700

    constant

---
+++ lib/core/constant/constant.dart
@@ -0,0 +1,6 @@
+const String BASE_URL = 'https://my-app.daisysunday.com';
+
+const String PREF_AUTH = 'pref-auth';
+const String PREF_ID = 'pred_id';
+const String PREF_NAME = 'pref_name';
+const String PREF_EMAIL = 'pref_email';
